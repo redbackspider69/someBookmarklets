248cca359ce4007f154c938f6abc3d307c14b392
Red-backspider <66238571+Red-backspider@users.noreply.github.com>
2023-06-20 15:50:19 +1000

    Create betterCalculator.js
    not working yet

---
+++ betterCalculator.js
@@ -0,0 +1,3 @@
+// not working (yet)
+$('head').append('<script id="calculator" type="module">import eval from "mathjs" var question = prompt("Calculator: Simply write a numeric expression, make sure to use the operators + - * /"); alert(question + " = " + math.eval(question));</script>');
+$('calculator').addEventListener("mouseover", function(), once);
