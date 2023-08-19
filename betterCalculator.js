2530846900d0dca48b96bafb405d04b1115291c7
redbackspider69 <66238571+Red-backspider@users.noreply.github.com>
2023-08-19 17:50:02 +1000

    Completed betterCalculator.js

--- betterCalculator.js
+++ betterCalculator.js
@@ -1,3 +1,75 @@
-// not working (yet)
-$('head').append('<script id="calculator" type="module">import eval from "mathjs" var question = prompt("Calculator: Simply write a numeric expression, make sure to use the operators + - * /"); alert(question + " = " + math.eval(question));</script>');
-$('calculator').addEventListener("mouseover", function(), once);
+(function() {
+  function calculateExpression(expression) {
+    const tokens = expression.match(/(\d+|\+|\-|\*|\/|\^)/g);
+
+    if (!tokens) {
+      return "Error: Invalid expression";
+    }
+
+    const precedence = {
+      "+": 1,
+      "-": 1,
+      "*": 2,
+      "/": 2,
+      "^": 3
+    };
+
+    const outputQueue = [];
+    const operatorStack = [];
+
+    for (const token of tokens) {
+      if (!isNaN(token)) {
+        outputQueue.push(parseFloat(token));
+      } else if (token in precedence) {
+        while (
+          operatorStack.length > 0 &&
+          precedence[operatorStack[operatorStack.length - 1]] >= precedence[token]
+        ) {
+          outputQueue.push(operatorStack.pop());
+        }
+        operatorStack.push(token);
+      }
+    }
+
+    while (operatorStack.length > 0) {
+      outputQueue.push(operatorStack.pop());
+    }
+
+    const evaluationStack = [];
+    for (const token of outputQueue) {
+      if (typeof token === "number") {
+        evaluationStack.push(token);
+      } else {
+        const b = evaluationStack.pop();
+        const a = evaluationStack.pop();
+        switch (token) {
+          case "+":
+            evaluationStack.push(a + b);
+            break;
+          case "-":
+            evaluationStack.push(a - b);
+            break;
+          case "*":
+            evaluationStack.push(a * b);
+            break;
+          case "/":
+            evaluationStack.push(a / b);
+            break;
+          case "^":
+            evaluationStack.push(Math.pow(a, b));
+            break;
+        }
+      }
+    }
+
+    if (evaluationStack.length === 1) {
+      return evaluationStack[0];
+    } else {
+      return "Error: Invalid expression";
+    }
+  }
+
+  var inputExpression = prompt("Mathematical Expression (use symbols such as * instead of X and / instead of ÷):");
+  var result = calculateExpression(inputExpression);
+  alert(`${inputExpression} = ${result}`);
+})();
